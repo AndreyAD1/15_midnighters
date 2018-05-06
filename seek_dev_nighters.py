7046a4942227fdf3eae1966a5858d0eb3a2b1a2a
Андрей Аракельянц <arakelyants.andrey@yandex.ru>
2018-05-06 16:55:27 +0300

    Improve the usage of datetime function and change variable names.

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -9,11 +9,11 @@ def load_attempts():
     while True:
         request_parameters = {'page': page}
         api_page = requests.get(attempts_link, request_parameters)
-        api_page_json = api_page.json()
-        for attempt in api_page_json['records']:
+        api_page_dict = api_page.json()
+        for attempt in api_page_dict['records']:
             yield attempt
         page += 1
-        if page > api_page_json['number_of_pages']:
+        if page > api_page_dict['number_of_pages']:
             break
 
 
@@ -31,15 +31,14 @@ def get_midnighters(attempts):
 
 
 def get_local_datetime_of_attempt(attempt_info):
-    utc_datetime_of_attempt = datetime.utcfromtimestamp(attempt_info['timestamp'])
     user_timezone = pytz.timezone(attempt_info['timezone'])
-    local_datetime_of_attempt = user_timezone.localize(utc_datetime_of_attempt)
-    return local_datetime_of_attempt
+    local_datetime = datetime.fromtimestamp(attempt_info['timestamp'], tz=user_timezone)
+    return local_datetime
 
 
 if __name__ == '__main__':
     attempts_generator = load_attempts()
     midnighters_list = get_midnighters(attempts_generator)
     print('The users who has ever sent an assignment between 0:00 and 6:00:')
-    for user in midnighters_list:
-        print(user)
+    for username in midnighters_list:
+        print(username)
