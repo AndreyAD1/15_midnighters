195f26d5fc6e6f704e168fb379152414fccb1db9
Андрей Аракельянц <arakelyants.andrey@yandex.ru>
2018-05-06 15:22:43 +0300

    Complete the script with a generator.

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,62 +1,45 @@
 import requests
 import pytz
-from datetime import datetime, time
-from collections import defaultdict
+from datetime import datetime
 
 
-def get_api_pages(page):
+def load_attempts():
     attempts_link = 'http://devman.org/api/challenges/solution_attempts'
-    request_parameters = {'page': page}
-    api_page = requests.get(attempts_link, request_parameters)
-    return api_page
-
-
-def get_all_attempts():
-    attempts = []
     page = 1
     while True:
-        api_page = get_api_pages(page)
-        if not api_page:
-            return attempts
-        page_attempts = api_page.json()['records']
-        attempts.extend(page_attempts)
+        request_parameters = {'page': page}
+        api_page = requests.get(attempts_link, request_parameters)
+        api_page_json = api_page.json()
+        for attempt in api_page_json['records']:
+            yield attempt
         page += 1
+        if page > api_page_json['number_of_pages']:
+            break
 
 
-def get_local_time_of_attempts(attempts):
-    local_time_of_attempts = defaultdict(list)
+def get_midnighters(attempts):
+    midnighters = []
+    midnight_hour = 0
+    morning_hour = 6
     for attempt in attempts:
-        utc_date_of_attempt = datetime.utcfromtimestamp(attempt['timestamp'])
-        user_timezone = pytz.timezone(attempt['timezone'])
-        local_date = user_timezone.localize(utc_date_of_attempt)
-        local_time = local_date.time()
-        user = attempt['username']
-        local_time_of_attempts[user].append(local_time)
-    return local_time_of_attempts
+        attempt_local_datetime = get_local_datetime_of_attempt(attempt)
+        if midnight_hour <= attempt_local_datetime.hour < morning_hour:
+            attempt_user = attempt['username']
+            if attempt_user not in midnighters:
+                midnighters.append(attempt_user)
+    return midnighters
 
 
-def check_night_time_of_attempt(attempts_time_list):
-    midnight = time()
-    night_end = time(hour=6)
-    for attempt_time in attempts_time_list:
-        if midnight < attempt_time <= night_end:
-            return True
-    return
-
-
-def get_midnighters(attempts_time_dict):
-    midnighters_list = []
-    for user in attempts_time_dict:
-        attempts_time_list = attempts_time_dict[user]
-        if check_night_time_of_attempt(attempts_time_list):
-            midnighters_list.append(user)
-    return midnighters_list
+def get_local_datetime_of_attempt(attempt_info):
+    utc_datetime_of_attempt = datetime.utcfromtimestamp(attempt_info['timestamp'])
+    user_timezone = pytz.timezone(attempt_info['timezone'])
+    local_datetime_of_attempt = user_timezone.localize(utc_datetime_of_attempt)
+    return local_datetime_of_attempt
 
 
 if __name__ == '__main__':
-    all_attempts = get_all_attempts()
-    local_time_of_users_attempts = get_local_time_of_attempts(all_attempts)
-    midnighters = get_midnighters(local_time_of_users_attempts)
-    print('Users who sent the assignment between 0:00 and 6:00:')
-    for user in midnighters:
+    attempts_generator = load_attempts()
+    midnighters_list = get_midnighters(attempts_generator)
+    print('The users who sent the assignment between 0:00 and 6:00:')
+    for user in midnighters_list:
         print(user)
